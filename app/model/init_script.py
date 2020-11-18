f42383d0aa5ba268d8ebf98d091f973ad063b330
Bohdan <32486313+bkovalevych@users.noreply.github.com>
2020-11-18 23:29:01 +0200

    change import again

--- app/model/init_script.py
+++ app/model/init_script.py
@@ -3,8 +3,8 @@ import pickle
 import os
 import sys
 sys.path.append(os.path.join(sys.path[0], 'model'))
-from .data_pre_processing import process_data
-from .train import train_model
+from data_pre_processing import process_data
+from train import train_model
 
 
 model_name = os.environ["MODEL_NAME"]
